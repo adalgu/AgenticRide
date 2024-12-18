161a763670d67988a3bea8f73466bdf61be3e531
adalgu <adalgu@gmail.com>
2024-12-18 15:44:38 +0900

    tool dev

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC<HeaderProps> = ({ apiKey, onResetApiKey }) => {
     <div className="header">
       <div className="header-title">
         <Book size={28} className="header-icon" />
-        <span className="app-name">Agentic Story Teller</span>
+        <span className="app-name">Agentic Ride by AZ2</span>
       </div>
       {/* API Key display - temporarily hidden
       <div className="header-api-key">
